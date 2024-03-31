1feb6083d6e84a592448e87e54b3a8b817883e62
LCcodefreak <lc.codefreak@gmail.com>
2024-03-31 19:41:15 +0100

    file(new):Ficheiro 1 - novo ficheiro "main.py"

---
+++ main.py
@@ -0,0 +1,16 @@
+# Projecto :
+# Nome - /Users/luiscordeiro/00_Pycharm/Pycharm_Projects/Python/TokioSchool/TokioSchool_ProjFinal_APP
+# Abril 2024
+
+# Ambiente Virtual :
+# Nome - /Users/luiscordeiro/00_Pycharm/Pycharm_Projects/Python/TokioSchool/TokioSchool_ProjFinal_APP/venv
+# Activar Ambiente Virtual :
+# Terminal > $source 00_Pycharm/Pycharm_Projects/Python/TokioSchool/TokioSchool_ProjFinal_APP/venv/bin/activate
+from tkinter import *
+
+master = Tk()
+master.title('Master')
+master.geometry('500x300+500+300')
+
+if __name__ == '__main__':
+    master.mainloop()
